9710f1206e1dddb2dfd6ecd01884343ff4650931
Jessica Craig <jesburt@ut.utm.edu>
2016-09-22 17:44:22 -0500

    CGContext -_- must find out how to use the move ( to: )

--- Learning-Swift/ViewController.swift
+++ Learning-Swift/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var permanentView: UIImageView!
    
     @IBOutlet weak var tempView: UIImageView!
+    
    
     
     var lastPoint = CGPoint.zero
@@ -21,7 +22,6 @@ class ViewController: UIViewController {
     var green: CGFloat = 0.0
     var blue: CGFloat = 0.0
     var brushWidth: CGFloat = 10.0
-    var opacity: CGFloat = 1.0
     var swiped = false
     
     let colors: [(CGFloat, CGFloat, CGFloat)] = [(0, 0, 0),  (105.0 / 255.0, 105.0 / 255.0, 105.0 / 255.0),
@@ -34,7 +34,28 @@ class ViewController: UIViewController {
     }
     
     
+    func touchesBegan(_ touches: Set<NSObject>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first as? UITouch{
+            lastPoint = touch.location(in: self.view)
+        }
+    }
     
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint){
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+    
+        //need to figure out this dang CGContext crap
+        func move(to point: fromPoint)
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+    
+    
+    }
+    
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
