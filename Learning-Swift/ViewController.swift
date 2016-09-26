b0504eb69f5396601e87cf83d86f7787fe727f78
Jessica Craig <jesburt@ut.utm.edu>
2016-09-26 14:30:35 -0500

    No errors pop up in code. Drawing won't work. v.v Cleaned up and changed some code to fit swift 3's demands.

--- Learning-Swift/ViewController.swift
+++ Learning-Swift/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     var green: CGFloat = 0.0
     var blue: CGFloat = 0.0
     var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
     var swiped = false
     
     let colors: [(CGFloat, CGFloat, CGFloat)] = [(0, 0, 0),  (105.0 / 255.0, 105.0 / 255.0, 105.0 / 255.0),
@@ -41,21 +42,61 @@ class ViewController: UIViewController {
         }
     }
     
-    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint){
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        
         UIGraphicsBeginImageContext(view.frame.size)
         let context = UIGraphicsGetCurrentContext()
         tempView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
-    
-        //need to figure out this dang CGContext crap
-        func move(to point: fromPoint)
-        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
-        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
         
-    
-    
+        
+        context!.move(to: fromPoint)
+        //CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        context!.addLine(to: toPoint)
+        //CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+       
+        context!.setLineCap(CGLineCap.round)
+        context!.setLineWidth(brushWidth)
+        context!.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+        context!.setBlendMode(CGBlendMode.normal)
+        
+        
+        context!.strokePath()
+        
+        
+        tempView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempView.alpha = opacity
+        UIGraphicsEndImageContext()
+        
     }
     
+    func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        swiped = true
+        if let touch = touches.first as? UITouch {
+            let currentPoint = touch.location(in: view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         
+        if !swiped {
+            // draw a single point
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(permanentView.frame.size)
+        permanentView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: CGBlendMode.normal, alpha: 1.0)
+        tempView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: CGBlendMode.normal, alpha: opacity)
+        permanentView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempView.image = nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
